6cc82411557aec08061883b82192db7b510e6f2b
VCP-coder <vaibhav249408@gmail.com>
2026-07-23 12:33:16 +0530

    Time: 1 ms (83.56%), Space: 42.8 MB (7.81%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,9 +1,5 @@
 class Solution {
     public int countCommas(int n) {
-        int count=0;
-       for(int i=n;i>=1000;i-- ) {
-           count++;
-       }
-    return count;
+        return n<1000?0:n-999;
     }
 }
