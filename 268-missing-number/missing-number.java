8c063108328b9442ce580eec476e00f00921d42f
VCP-coder <vaibhav249408@gmail.com>
2026-07-29 22:50:52 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (97.76%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,11 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n=nums.length;
-       int total=n*(n+1)/2 ;
-       int sum=0;
-       for(int num:nums){
-        sum+=num;
-       }
-    return total-sum;
+        int sum=n*(n+1)/2;
+        int total=0;
+        for(int num:nums){
+            total+=num;
+        }
+    return sum-total;
     }
 }
