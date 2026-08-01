34930e5ae34978bf95181be8bcec846b518dab84
VCP-coder <vaibhav249408@gmail.com>
2026-08-01 23:30:41 +0530

    Time: 1 ms (99.88%), Space: 47.4 MB (28.25%) - LeetHub

--- 3033-modify-the-matrix/3033-modify-the-matrix.java
+++ 3033-modify-the-matrix/3033-modify-the-matrix.java
@@ -2,22 +2,20 @@ class Solution {
     public int[][] modifiedMatrix(int[][] matrix) {
         int row=matrix.length;
         int col=matrix[0].length;
+        int maxt[]=new int[col];
         int k=0;
-        int maxcol[]=new int[col];
         for(int j=0;j<col;j++){
             int max=0;
             for(int i=0;i<row;i++){
-                if(matrix[i][j]>max){
-                    max=matrix[i][j];
-                }
+                max=Math.max(max,matrix[i][j]);
             }
-        maxcol[k]=max;
+        maxt[k]=max;
         k++;
         }
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 if(matrix[i][j]==-1){
-                    matrix[i][j]=maxcol[j];
+                    matrix[i][j]=maxt[j];
                 }
             }
         }
