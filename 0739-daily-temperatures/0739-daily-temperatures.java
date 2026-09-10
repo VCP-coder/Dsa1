521da4747ac02184a1ed3a5ffc6c7f933e50f9ef
VCP-coder <vaibhav249408@gmail.com>
2026-09-10 23:16:09 +0530

    Time: 85 ms (5.31%), Space: 107.6 MB (33.19%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int res[]=new int[temperatures.length];
+        Stack<Integer> st=new Stack<>(); 
+        for(int day=0;day<temperatures.length;day++){
+            while(!st.isEmpty() && temperatures[day]>temperatures[st.peek()]){
+                res[st.peek()]=day-st.peek();
+                st.pop();
+            }
+            st.push(day);
+        }
+    return res;
+    }
+}
