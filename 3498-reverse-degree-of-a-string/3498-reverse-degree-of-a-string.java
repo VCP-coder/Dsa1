274dad22e692f20754ff6333b78150fd8e1cac3e
VCP-coder <vaibhav249408@gmail.com>
2026-09-20 15:28:13 +0530

    Time: 1 ms (100%), Space: 44.1 MB (50.47%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -1,12 +1,10 @@
 class Solution {
     public int reverseDegree(String s) {
-        int ans=0;
+        int res=0;
         for(int i=0;i<s.length();i++){
-         
-           int val=(int)s.charAt(i);
-           int fval=Math.abs(val-123);
-           ans=ans+fval*(i+1);
+            int val=Math.abs((int)s.charAt(i)-123);
+            res+=val*(i+1);
         }
-    return ans;
+    return res;
     }
 }
